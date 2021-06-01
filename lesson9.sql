7fc4f6799f1936d880545bc215bf67e2fc9c832a
Kamenev Vladimir <kvv23@tut.by>
2021-06-01 23:18:51 +0300

    Добавлены задачи 1 и 2 по теме Хранимые процедуры и функции, триггеры

--- lesson9.sql
+++ lesson9.sql
@@ -1,12 +1,15 @@
 
 -- Урок 9.
 
+-- Задание по теме "Транзакции, переменные, представления"
+
 -- Задание 1. В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. 
 -- Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.
 USE shop;
-truncate TABLE sample.users;
+-- truncate TABLE sample.users;
 START TRANSACTION;	
-	INSERT INTO sample.users SELECT * FROM shop.users su WHERE su.id = 1;
+	INSERT INTO sample.users SELECT NULL, su.name,su.birthday_at,su.created_at,su.updated_at FROM shop.users su WHERE su.id = 1;
+	DELETE FROM shop.users su WHERE su.id = 1;
 COMMIT;
 
 -- Задание 2. Создайте представление, которое выводит название name товарной позиции из таблицы products и 
@@ -74,4 +77,55 @@ FROM
 aug_days a
 LEFT JOIN 
 aug_date ON @start_date + INTERVAL a.days DAY = aug_date.created_at;
+
+-- Задание по теме "Хранимые процедуры и функции, триггеры"
+
+-- Задание 1. Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток. 
+-- С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", 
+-- с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+
+DROP FUNCTION IF EXISTS hello;
+
+delimiter //
+CREATE FUNCTION hello()
+RETURNS TEXT DETERMINISTIC
+BEGIN 
+	DECLARE hel TEXT DEFAULT "Добрый вечер";
+	
+	IF (curtime() < "06:00:00") THEN 
+		SET hel = "Доброй ночи";
+	ELSEIF (curtime() < "12:00:00") THEN 
+		SET hel = "Доброе утро";
+	ELSEIF (curtime() < "18:00:00") THEN 
+		SET hel = "Добрый день";
+	END IF;
+	RETURN hel;
+END//
+delimiter ;
+
+SELECT hello();
+
+
+-- Задание 2. В таблице products есть два текстовых поля: name с названием товара и description с его описанием. 
+-- Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема. 
+-- Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. 
+-- При попытке присвоить полям NULL-значение необходимо отменить операцию.
+
+DROP TRIGGER IF EXISTS insert_product;
+
+delimiter //
+
+CREATE TRIGGER insert_product BEFORE INSERT ON products
+
+FOR EACH ROW 
+
+BEGIN 
+	IF (isnull(NEW.name) AND isnull(NEW.description)) THEN
+		SIGNAL SQLSTATE '45000' SET message_text = "Наименование товара и его описание не могут одновременно иметь значение null";
+	END IF;
+END//
+delimiter ;
+
+INSERT INTO products (name, description, price,catalog_id) VALUES 
+(NULL,NULL,1000,20);
 
